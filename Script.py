614f33c1cbfcfeb9c3709349d86650519302cb11
AlexVesk <47559000+AlexVesk@users.noreply.github.com>
2019-11-24 22:33:58 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,40 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 24 00:15:41 2019
-
-@author: Alex
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 22 10:20:53 2019
-@author: uocvc
-"""
-
-
-import os
-directory = 'C:/Users/Alex/Desktop/data-science-bowl-2019/Data'
-os.chdir(directory)
-import pandas as pd
-import numpy as np
-
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
-
-
-train = pd.read_csv('train.csv')
-train_labels = pd.read_csv('train_labels.csv')
-specs = pd.read_csv('specs.csv')
-test = pd.read_csv('test.csv')
-
-
-# =============================================================================
-# train.head()
-# train_labels.head()
-# =============================================================================
 
 
 def Manipulate_Baselile_Data(train):
@@ -48,15 +11,11 @@ def Manipulate_Baselile_Data(train):
     Assessments.loc[Assessments['event_data'].str.contains('true') & Assessments['Attempt'] == 1 ,'IsAttemptSuccessful'] = 1
     Assessments['timestamp'] = pd.to_datetime(Assessments['timestamp'] , format="%Y-%m-%d %H:%M")
     
-    Assessments['stdGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.std)
+    Assessments['MeanEventTime'] = Assessments.groupby(['installation_id','event_id','game_session'])['game_time'].transform(np.mean)
+    Assessments['MaxEventTime'] = Assessments.groupby(['installation_id','event_id','game_session'])['game_time'].transform(np.max)
     Assessments['MaxGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.max)
-    Assessments['MeanGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.mean)
-    
-    
-    Assessments['stdGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.std)
     Assessments['MaxGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.max)
-    Assessments['MeanGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.mean)
-    
+    Assessments = Assessments.sort_values('timestamp')
     Assessments= Assessments[Assessments.Attempt == 1]
     Model_Base = Assessments.drop_duplicates()
     Model_Base['time'] = Model_Base['timestamp'].dt.strftime('%H:%M')
@@ -69,22 +28,14 @@ def Manipulate_Baselile_Data(train):
     Model_Base.loc[Model_Base.time.between('00:00', '05:00', inclusive = True) ,"PartOfDay"] = 'Night'
     Model_Base['title'] = Model_Base['title'].str.rstrip(' (Assessment)')
     Model_Base = Model_Base.set_index(['installation_id','game_session'])
-    Model_Base= Model_Base[['title','PartOfDay','stdGameTime','MaxGameTime','MeanGameTime','stdGameEventCount','MaxGameEventCount','MeanGameEventCount']]
+    Model_Base= Model_Base[['title','PartOfDay','world','MeanEventTime','MaxEventTime','MaxGameTime','MaxGameEventCount']]
     Model_Base = pd.concat([Model_Base, pd.get_dummies(Model_Base['PartOfDay'])] ,axis=1)
     del Model_Base['PartOfDay']
+    Model_Base = pd.concat([Model_Base, pd.get_dummies(Model_Base['world'])] ,axis=1)
+    del Model_Base['world']
+    #Model_Base['NumOfPreviousGames'] = Assessments.groupby(['installation_id'])['game_session'].transform('count')
     return Model_Base.drop_duplicates()
 
 
 
 ModelBase = Manipulate_Baselile_Data(train)
-
-# Attach ground truth on train
-train_labels['title'] = train_labels['title'].str.rstrip(' (Assessment)')
-Accuracy = train_labels[['installation_id', 'title', 'game_session','accuracy']]
-ModelBaseWithAccuracy = pd.merge(Accuracy, ModelBase,  how='right', on=['installation_id', 'title', 'game_session'])
-ModelBaseWithAccuracy = pd.concat([ModelBaseWithAccuracy, pd.get_dummies(ModelBaseWithAccuracy['title'])] ,axis=1)
-del ModelBaseWithAccuracy['title']
-ModelBaseWithAccuracy.to_csv('ModelBaseWithAccuracy.csv')
-
-
-
