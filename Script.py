7d38c757a5c7e28548be0d03875fcbd24574b005
AlexVesk <47559000+AlexVesk@users.noreply.github.com>
2019-11-24 21:10:51 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,6 +28,7 @@ pd.set_option('display.width', 1000)
 train = pd.read_csv('train.csv')
 train_labels = pd.read_csv('train_labels.csv')
 specs = pd.read_csv('specs.csv')
+test = pd.read_csv('test.csv')
 
 
 # =============================================================================
@@ -36,7 +37,7 @@ specs = pd.read_csv('specs.csv')
 # =============================================================================
 
 
-def Manipulate_Data(train, train_labels):
+def Manipulate_Baselile_Data(train, train_labels):
     train_Assessments = train.loc[train.type=='Assessment']
     Assessments = pd.merge(train_Assessments, train_labels, how='left', on=['installation_id', 'title', 'game_session'])
     Assessments['Attempt'] = 0
@@ -47,11 +48,18 @@ def Manipulate_Data(train, train_labels):
     Assessments.loc[Assessments['event_data'].str.contains('false') & Assessments['Attempt'] == 1 ,'IsAttemptSuccessful'] = 0
     Assessments.loc[Assessments['event_data'].str.contains('true') & Assessments['Attempt'] == 1 ,'IsAttemptSuccessful'] = 1
     Assessments['timestamp'] = pd.to_datetime(Assessments['timestamp'] , format="%Y-%m-%d %H:%M")
-    Assessments['NumOfPastAttempts'] = Assessments.groupby(['installation_id','game_session', 'title'])['Attempt'].transform(np.cumsum) - 1
-    Assessments['IsSuccessfull'] = Assessments.groupby(['installation_id','game_session', 'title'])['IsAttemptSuccessful'].transform(np.cumsum)
+    
+    Assessments['stdGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.std)
+    Assessments['MaxGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.max)
+    Assessments['MeanGameTime'] = Assessments.groupby(['installation_id','game_session'])['game_time'].transform(np.mean)
+    
+    
+    Assessments['stdGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.std)
+    Assessments['MaxGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.max)
+    Assessments['MeanGameEventCount'] = Assessments.groupby(['installation_id','game_session'])['event_count'].transform(np.mean)
+    
     Assessments= Assessments[Assessments.Attempt == 1]
-    Assessments['AttemptNumber'] = Assessments.groupby(['installation_id','game_session']).cumcount()+1
-    Model_Base = Assessments[['installation_id', 'game_session','AttemptNumber','IsSuccessfull','NumOfPastAttempts','game_time','event_count','title','timestamp','accuracy','accuracy_group']].drop_duplicates()
+    Model_Base = Assessments.drop_duplicates()
     Model_Base['time'] = Model_Base['timestamp'].dt.strftime('%H:%M')
     Model_Base['date'] = Model_Base['timestamp'].dt.date
     del Model_Base['timestamp']
@@ -62,13 +70,22 @@ def Manipulate_Data(train, train_labels):
     Model_Base.loc[Model_Base.time.between('00:00', '05:00', inclusive = True) ,"PartOfDay"] = 'Night'
     Model_Base['title'] = Model_Base['title'].str.rstrip(' (Assessment)')
     Model_Base = Model_Base.set_index(['installation_id','game_session'])
-    Model_Base= Model_Base[['accuracy','AttemptNumber','game_time','event_count','title','PartOfDay']]
+    Model_Base= Model_Base[['accuracy','title','PartOfDay','stdGameTime','MaxGameTime','MeanGameTime','stdGameEventCount','MaxGameEventCount','MeanGameEventCount']]
     Model_Base = pd.concat([Model_Base, pd.get_dummies(Model_Base['PartOfDay'])] ,axis=1)
     del Model_Base['PartOfDay']
     Model_Base = pd.concat([Model_Base, pd.get_dummies(Model_Base['title'])] ,axis=1)
     del Model_Base['title']
-    return Model_Base
+    return Model_Base.drop_duplicates()
 
 
-ModelBase = Manipulate_Data(train, train_labels)
+ModelBase = Manipulate_Baselile_Data(train, train_labels)
 ModelBase.to_csv('ModelBase.csv')
+
+
+
+train_Assessments = train.loc[train.type=='Assessment']
+train_Assessments.groupby('installation_id')['game_session'].count()
+
+test_Assessments = test.loc[test.type=='Assessment']
+test_Assessments.groupby('installation_id')['game_session'].count()
+
