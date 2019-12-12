7b8e87a1d1aad4aedd3704dc7a46c25f2f29ea9d
Alex.Veskoukis <alex.veskoukis@hotmail.gr>
2019-12-12 16:48:46 +0200

    train and test superlearner

--- Train_and_Test.py
+++ Train_and_Test.py
@@ -416,6 +416,24 @@ def get_all_but_last_assessment(data):
     Assess = Assess.loc[Assess.To_Predict != 1, :]
     return Assess[['installation_id', 'game_session', 'accuracy_group']]
 
+def get_vists_per_title(data):
+    import pandas as pd
+    title_slice5 =pd.pivot_table(data[['installation_id', 'game_session', 'type', 'title', 'Game_Session_Order']],
+                       index=['installation_id', 'game_session', 'type', 'Game_Session_Order'],
+                       columns='title',
+                       aggfunc=len,
+                       fill_value=0).reset_index().sort_values(['installation_id', 'Game_Session_Order'])
+    titlecols = data.title.unique()
+    title_slice5[["visits_" + title for title in titlecols]] = \
+        title_slice5.groupby('installation_id')[titlecols].transform(np.nancumsum)
+    title_slice5_assessments = title_slice5[title_slice5.type == 'Assessment']
+
+    cols = ["visits_" + title for title in titlecols]
+    cols.extend(['installation_id', 'game_session'])
+    titles_played = title_slice5_assessments[cols]
+    return titles_played
+
+
 
 def create_features(data):
     trainTitles = data['title'].unique()
@@ -495,6 +513,10 @@ def create_features(data):
     print('Level_reached')
     world_time_gametitles_dummies = create_world_time_assesstitle_Dummies(data)
     print('world_time_gametitles_dummies')
+
+    Titles = get_vists_per_title(slice1)
+
+
     Sets = [Number_of_games_played_per_type,
             Time_spent_on_games_metrics,
             world_time_gametitles_dummies,
@@ -518,8 +540,9 @@ FinalTrain = pd.merge(Final,
 
 FinalTrain = FinalTrain.set_index(['installation_id', 'game_session'])
 
-X_train = FinalTrain.drop('accuracy_group', axis = 1)
-Y_train = FinalTrain['accuracy_group'].astype(int)
+X_train = np.asarray(FinalTrain.drop('accuracy_group', axis = 1))
+
+Y_train = np.asarray(FinalTrain['accuracy_group']).astype(int)
 
 
 
@@ -541,40 +564,252 @@ X_test = Test_set_full.set_index(['installation_id', 'game_session'])
 Y_test = Test_Set['accuracy_group'].astype(int)
 
 
-################################################# Modelling ############################################################
-from sklearn.metrics import accuracy_score
-cols = X_train.columns
-cols = ['Total_Games_played', 'Clips__played', 'Games_played',
-       'Assessments_played', 'Activities_played', 'Total_Time_spent',
-       'Time_spent_on_Activities', 'Time_spent_on_Games',
-       'Time_spent_on_Assessments', 'zeros', 'ones', 'twos', 'threes',
-        'Game_Session_Order', 'Past_Total_Attempts',
-        'Past_Total_Successes', 'Past_Total_Fails', 'Past_Assessments_Played',
-        'Past_Assessment_Session_Time', 'Past_Assessment_NumberOfEvents',
-        'CRYSTALCAVES', 'MAGMAPEAK', 'TREETOPCITY',
-        'Bird Measurer', 'Cart Balancer', 'Cauldron Filler', 'Chest Sorter','Mushroom Sorter',
-'cummean_ag',
-        'cumAverageTime', 'cumAverageEvents', 'cumsdTime', 'cumsdEvents'
-        ]
+################################################# Modelling  things ####################################################
+from sklearn.metrics import accuracy_score, cohen_kappa_score
+from sklearn.neural_network import MLPClassifier
 
 
-import itertools
 
-
-ColumnList = []
-for L in range(0, len(cols)+1):
-    for subset in itertools.combinations(cols, L):
-        print(subset)
-        ColumnList.append(subset)
-
-
-model = RandomForestClassifier(n_estimators= 8, n_jobs=-1, random_state=42)
+model = RandomForestClassifier(n_estimators= 100, n_jobs=-1, random_state=42)
 model.fit(X_train[cols], Y_train)
+
+# example of a super learner model for binary classification
+from numpy import hstack
+from numpy import vstack
+from numpy import asarray
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+
+# create a list of base-models
+def get_models():
+    models = list()
+    models.append(LogisticRegression(solver='liblinear'))
+    models.append(DecisionTreeClassifier())
+    models.append(SVC(gamma='scale', probability=True))
+    models.append(GaussianNB())
+    models.append(KNeighborsClassifier())
+    models.append(AdaBoostClassifier())
+    models.append(BaggingClassifier(n_estimators=10))
+    models.append(RandomForestClassifier(n_estimators=10))
+    models.append(ExtraTreesClassifier(n_estimators=10))
+    return models
+
+
+# collect out of fold predictions form k-fold cross validation
+def get_out_of_fold_predictions(X, y, models):
+    meta_X, meta_y = list(), list()
+    # define split of data
+    kfold = KFold(n_splits=10, shuffle=True)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(X):
+        fold_yhats = list()
+        # get data
+        train_X, test_X = X[train_ix], X[test_ix]
+        train_y, test_y = y[train_ix], y[test_ix]
+        meta_y.extend(test_y)
+        # fit and make predictions with each sub-model
+        for model in models:
+            model.fit(train_X, train_y)
+            yhat = model.predict_proba(test_X)
+            # store columns
+            fold_yhats.append(yhat)
+        # store fold yhats as columns
+        meta_X.append(hstack(fold_yhats))
+    return vstack(meta_X), asarray(meta_y)
+
+
+# fit all base models on the training dataset
+def fit_base_models(X, y, models):
+    for model in models:
+        model.fit(X, y)
+
+
+# fit a meta model
+def fit_meta_model(X, y):
+    model = LogisticRegression(solver='liblinear')
+    model.fit(X, y)
+    return model
+
+
+# evaluate a list of models on a dataset
+def evaluate_models(X, y, models):
+    for model in models:
+        yhat = model.predict(X)
+        acc = accuracy_score(y, yhat)
+        print('%s: %.3f' % (model.__class__.__name__, acc * 100))
+
+
+# make predictions with stacked model
+def super_learner_predictions(X, models, meta_model):
+    meta_X = list()
+    for model in models:
+        yhat = model.predict_proba(X)
+        meta_X.append(yhat)
+    meta_X = hstack(meta_X)
+    # predict
+    return meta_model.predict(meta_X)
+
+
+# create the inputs and outputs
+X, y = make_blobs(n_samples=1000, centers=2, n_features=100, cluster_std=20)
+# split
+X, X_val, y, y_val = train_test_split(X, y, test_size=0.50)
+print('Train', X.shape, y.shape, 'Test', X_val.shape, y_val.shape)
+# get models
+models = get_models()
+# get out of fold predictions
+meta_X, meta_y = get_out_of_fold_predictions(X, y, models)
+print('Meta ', meta_X.shape, meta_y.shape)
+# fit base models
+fit_base_models(X, y, models)
+# fit the meta model
+meta_model = fit_meta_model(meta_X, meta_y)
+# evaluate base models
+evaluate_models(X_val, y_val, models)
+# evaluate meta model
+yhat = super_learner_predictions(X_val, models, meta_model)
+print('Super Learner: %.3f' % (accuracy_score(y_val, yhat) * 100))
+
+# example of a super learner model for binary classification
+from numpy import hstack
+from numpy import vstack
+from numpy import asarray
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+
+# create a list of base-models
+def get_models():
+    models = list()
+    models.append(LogisticRegression(solver='liblinear'))
+    models.append(DecisionTreeClassifier())
+    models.append(SVC(gamma='scale', probability=True))
+    models.append(GaussianNB())
+    models.append(KNeighborsClassifier())
+    models.append(AdaBoostClassifier())
+    models.append(BaggingClassifier(n_estimators=10))
+    models.append(RandomForestClassifier(n_estimators=10))
+    models.append(ExtraTreesClassifier(n_estimators=10))
+    return models
+
+
+# collect out of fold predictions form k-fold cross validation
+def get_out_of_fold_predictions(X, y, models):
+    meta_X, meta_y = list(), list()
+    # define split of data
+    kfold = KFold(n_splits=10, shuffle=True)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(X):
+        fold_yhats = list()
+        # get data
+        train_X, test_X = X[train_ix], X[test_ix]
+        train_y, test_y = y[train_ix], y[test_ix]
+        meta_y.extend(test_y)
+        # fit and make predictions with each sub-model
+        for model in models:
+            model.fit(train_X, train_y)
+            yhat = model.predict_proba(test_X)
+            # store columns
+            fold_yhats.append(yhat)
+        # store fold yhats as columns
+        meta_X.append(hstack(fold_yhats))
+    return vstack(meta_X), asarray(meta_y)
+
+
+# fit all base models on the training dataset
+def fit_base_models(X, y, models):
+    for model in models:
+        model.fit(X, y)
+
+
+# fit a meta model
+def fit_meta_model(X, y):
+    model = LogisticRegression(solver='liblinear')
+    model.fit(X, y)
+    return model
+
+
+# evaluate a list of models on a dataset
+def evaluate_models(X, y, models):
+    for model in models:
+        yhat = model.predict(X)
+        acc = accuracy_score(y, yhat)
+        print('%s: %.3f' % (model.__class__.__name__, acc * 100))
+
+
+# make predictions with stacked model
+def super_learner_predictions(X, models, meta_model):
+    meta_X = list()
+    for model in models:
+        yhat = model.predict_proba(X)
+        meta_X.append(yhat)
+    meta_X = hstack(meta_X)
+    # predict
+    return meta_model.predict(meta_X)
+
+
+
+
+
+
+# # create the inputs and outputs
+# X, y = make_blobs(n_samples=1000, centers=2, n_features=100, cluster_std=20)
+# # split
+# X, X_val, y, y_val = train_test_split(X, y, test_size=0.50)
+# print('Train', X.shape, y.shape, 'Test', X_val.shape, y_val.shape)
+# get models
+models = get_models()
+# get out of fold predictions
+meta_X, meta_y = get_out_of_fold_predictions(X_train, Y_train, models)
+print('Meta ', meta_X.shape, meta_y.shape)
+# fit base models
+fit_base_models(X_train, Y_train, models)
+# fit the meta model
+meta_model = fit_meta_model(meta_X, meta_y)
+# evaluate base models
+evaluate_models(X_test, Y_test, models)
+# evaluate meta model
+yhat = super_learner_predictions(X_test, models, meta_model)
+print('Super Learner: %.3f' % (accuracy_score(Y_test, yhat) * 100))
+
+
+
+
+
+
+
+
+
+
+
+
 
 Y_pred_test = model.predict(X_test[cols])
 accuracy_score(Y_test, Y_pred_test)
-quadratic_weighted_kappa(Y_test, Y_pred_test)
-
+cohen_kappa_score(Y_test, Y_pred_test, weights='quadratic')
 submission = pd.DataFrame({"installation_id": X_test.reset_index(1).index.values,
                            "accuracy_group": Y_pred_test})
 submission.accuracy_group.value_counts()
