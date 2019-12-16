11cf8f09d597b307dd39334e5b3dc8ce99342977
Alex.Veskoukis <alex.veskoukis@hotmail.gr>
2019-12-16 13:28:20 +0200

    train and test superlearner

--- Train_and_Test.py
+++ Train_and_Test.py
@@ -51,21 +51,18 @@ def get_previous_ac_metrics(data):
     Assess['Past_Assessment_ag'] = round(Assess.groupby('installation_id')['accuracy_group'].shift(1, fill_value=0))
     Assess['Past_Assessment_att'] = round(Assess.groupby('installation_id')['Attempts'].shift(1, fill_value=0))
     Assess['Past_Assessment_succ'] = round(Assess.groupby('installation_id')['Success'].shift(1, fill_value=0))
-
     Assess['cummean_ag'] = Assess.groupby('installation_id')['Past_Assessment_ag'].transform(
         lambda x: x.expanding().mean())
     Assess['cummean_att'] = Assess.groupby('installation_id')['Past_Assessment_att'].transform(
         lambda x: x.expanding().mean())
     Assess['cummean_succ'] = Assess.groupby('installation_id')['Past_Assessment_succ'].transform(
         lambda x: x.expanding().mean())
-
     Assess['cumstd_ag'] = Assess.groupby('installation_id')['Past_Assessment_ag'].transform(
         lambda x: x.expanding().std())
     Assess['cumstd_att'] = Assess.groupby('installation_id')['Past_Assessment_att'].transform(
         lambda x: x.expanding().std())
     Assess['cumstd_succ'] = Assess.groupby('installation_id')['Past_Assessment_succ'].transform(
         lambda x: x.expanding().std())
-
     Assess['cumstd_ag'].fillna(0, inplace=True)
     Assess['cumstd_att'].fillna(0, inplace=True)
     Assess['cumstd_succ'].fillna(0, inplace=True)
@@ -91,7 +88,8 @@ def get_previous_ac_metrics(data):
          'cummean_succ',
          'cumstd_ag',
          'cumstd_att',
-         'cumstd_succ']]
+         'cumstd_succ'
+         ]]
     AssessFinal = pd.merge(Assess,Assess2, on = ['installation_id', 'game_session'], how= 'inner')
     return AssessFinal
 
@@ -127,26 +125,28 @@ def get_past_assessment_time_events_and_metrics(data):
         shift(1, fill_value=0)
     slice3['Past_Assessment_NumberOfEvents'] = slice3.groupby('installation_id')['Assessment_NumberOfEvents']. \
         shift(1, fill_value=0)
-    slice3 = slice3.assign(
-        cumAverageTime=slice3.groupby('installation_id', sort=False)['Past_Assessment_Session_Time']. \
-                           transform(lambda x: x.expanding().mean()))
-    slice3 = slice3.assign(
-        cumAverageEvents=slice3.groupby('installation_id', sort=False)['Past_Assessment_NumberOfEvents']. \
-            transform(lambda x: x.expanding().mean()))
-    slice3 = slice3.assign(cumsdTime=slice3.groupby('installation_id', sort=False)['Past_Assessment_Session_Time']. \
-                           transform(lambda x: x.expanding().std()))
-    slice3['cumsdTime'].fillna(0, inplace=True)
-    slice3 = slice3.assign(cumsdEvents=slice3.groupby('installation_id', sort=False)['Past_Assessment_NumberOfEvents']. \
-                           transform(lambda x: x.expanding().std()))
-    slice3['cumsdEvents'].fillna(0, inplace=True)
+    # slice3 = slice3.assign(
+    #     cumAverageTime=slice3.groupby('installation_id', sort=False)['Past_Assessment_Session_Time']. \
+    #                        transform(lambda x: x.expanding().mean()))
+    # slice3 = slice3.assign(
+    #     cumAverageEvents=slice3.groupby('installation_id', sort=False)['Past_Assessment_NumberOfEvents']. \
+    #         transform(lambda x: x.expanding().mean()))
+    # slice3 = slice3.assign(cumsdTime=slice3.groupby('installation_id', sort=False)['Past_Assessment_Session_Time']. \
+    #                        transform(lambda x: x.expanding().std()))
+    # slice3['cumsdTime'].fillna(0, inplace=True)
+    # slice3 = slice3.assign(cumsdEvents=slice3.groupby('installation_id', sort=False)['Past_Assessment_NumberOfEvents']. \
+    #                        transform(lambda x: x.expanding().std()))
+    # slice3['cumsdEvents'].fillna(0, inplace=True)
     slice3 = slice3[
         ['installation_id', 'game_session',
          'Past_Assessment_Session_Time',
-         'Past_Assessment_NumberOfEvents',
-         'cumAverageTime',
-         'cumAverageEvents',
-         'cumsdTime',
-         'cumsdEvents']]
+         'Past_Assessment_NumberOfEvents'
+         #    ,
+         # 'cumAverageTime',
+         # 'cumAverageEvents',
+         # 'cumsdTime',
+         # 'cumsdEvents'
+         ]]
     return (slice3)
 
 
@@ -285,7 +285,6 @@ def get_cumulative_time_spent_on_types(data):
     type_slice2_assessments['Std_Time_spent_on_games'] = \
     type_slice2_assessments.groupby('installation_id')['Total_Time_spent'].transform(lambda x: x.expanding().std())
     type_slice2_assessments = type_slice2_assessments.reset_index()
-    type_slice2_assessments['Std_Time_spent_on_games'] = type_slice2_assessments['Std_Time_spent_on_games'].fillna(0)
     type_slice2_assessments = type_slice2_assessments.loc[:,
     ['installation_id', 'game_session',
     'Total_Time_spent',
@@ -295,10 +294,10 @@ def get_cumulative_time_spent_on_types(data):
     'Average_Time_spent_on_Activities',
     'Average_Time_spent_on_Games',
     'Average_Time_spent_on_Assessments',
-    'Average_Time_spent_on_games',
-    'Std_Time_spent_on_games'
+    'Average_Time_spent_on_games'
     ]].drop_duplicates()
     return type_slice2_assessments
+
 
 def get_time_spent_on_diffrent_worlds(data):
     import pandas as pd
@@ -364,7 +363,18 @@ def substract_level(data):
     return Level_slice4
 
 
+
+
+
 def create_world_time_assesstitle_Dummies(data):
+    def dummyfy(df, colname):
+        df[colname] = pd.Categorical(df[colname])
+        dummies = pd.get_dummies(df[colname])
+        dummies.columns = [str(i) + '_' + colname for i in dummies.columns]
+        df = pd.concat([df,dummies], axis=1)
+        del df[colname]
+        return df
+
     import pandas as pd
     Assessments = data[data.type == 'Assessment'].copy()
     Assessments['timestamp'] = pd.to_datetime(Assessments['timestamp'], format="%Y-%m-%d %H:%M")
@@ -374,16 +384,13 @@ def create_world_time_assesstitle_Dummies(data):
     del Assessments['timestamp']
     Assessments['title'] = Assessments['title'].str.rstrip(' (Assessment)')
     Assessments = Assessments.set_index(['installation_id', 'game_session'])
-    Assessments = Assessments[['title', 'world', 'month', 'hour', 'year', 'dayofweek']]
-    Assessments['title'] = pd.Categorical(Assessments['title'])
-    Assessments['world'] = pd.Categorical(Assessments['world'])
-    Assessments = pd.concat([Assessments, pd.get_dummies(Assessments['world'])], axis=1)
-    del Assessments['world']
-    Assessments = pd.concat([Assessments, pd.get_dummies(Assessments['title'])], axis=1)
-    del Assessments['title']
+    Assessments = Assessments[['title', 'world', 'hour',  'month', 'year', 'dayofweek']]
+    Assessments = dummyfy(df=Assessments, colname='title')
+    Assessments = dummyfy(df=Assessments, colname='world')
+    Assessments = dummyfy(df=Assessments, colname='hour')
+    Assessments = dummyfy(df=Assessments, colname='dayofweek')
     Assessments = Assessments.reset_index()
     Assessments = Assessments.drop_duplicates()
-    Assessments = Assessments[Assessments.hour == Assessments.groupby(['installation_id', 'game_session'])['hour'].transform('min')]
     return Assessments
 
 
@@ -457,12 +464,12 @@ def get_event_code_history(data):
     event_code_history = Events.drop(['order','type'], axis=1)
     return event_code_history
 
-def get_success_rate_by_world:
-    import pandas as pd
-    df = data[data.type == 'Assessment'].copy()
-    df['Success_Rate'] = df.groupby(['installation_id', 'game_session', 'world'])['Attempt', 'IsAttemptSuccessful'].\
-        apply(lambda x,y: sum(y)/sum(x))
-    return bla
+# def get_success_rate_by_world:
+#     import pandas as pd
+#     df = data[data.type == 'Assessment'].copy()
+#     df['Success_Rate'] = df.groupby(['installation_id', 'game_session', 'world'])['Attempt', 'IsAttemptSuccessful'].\
+#         apply(lambda x,y: sum(y)/sum(x))
+#     return bla
 
 def calculate_accuracy_group(data):
         dt = data.copy()
@@ -485,14 +492,11 @@ def calculate_accuracy_group(data):
 
 
 def create_features(data):
-
     data['timestamp'] = pd.to_datetime(data['timestamp'], format="%Y-%m-%d %H:%M")
     data = data.sort_values(['installation_id', 'timestamp', 'game_session'], ascending=[True, True, True])
     Inst_Group = data.groupby('installation_id')
     Inst_Game_Group = data.groupby(['installation_id', 'game_session'])
     # initial measures
-
-
     data['Total_Game_Session_Time'] = Inst_Game_Group['game_time'].transform(np.max)
     data['Total_Game_Session_Events'] = Inst_Game_Group['event_count'].transform(np.max)
     data['Assessments_played_Counter'] = data[data.type == 'Assessment'].groupby('installation_id')[
@@ -508,11 +512,8 @@ def create_features(data):
     SuccessfulAttemptIndicator = data['event_data'].str.contains('true') & AttemptIndicator
     data['IsAttemptSuccessful'] = 0
     data.loc[SuccessfulAttemptIndicator, 'IsAttemptSuccessful'] = 1
-
     ag = calculate_accuracy_group(data)
-
     data['Cumulative_Attempts'] = Inst_Group['Attempt'].transform(np.cumsum)
-    data['Cumulative_Successes'] = Inst_Group['IsAttemptSuccessful'].transform(np.nancumsum)
     data['Cumulative_Successes'] = Inst_Group['IsAttemptSuccessful'].transform(np.nancumsum)
     data['Cumulative_Fails'] = data['Cumulative_Attempts'] - data['Cumulative_Successes']
     data['Assessment_Session_Time'] = data[data.type == 'Assessment'].groupby(['installation_id', 'game_session'])[
@@ -529,29 +530,31 @@ def create_features(data):
     past_assessment_time_events_and_metrics = get_past_assessment_time_events_and_metrics(data)
     print('past_assessment_time_events_and_metrics')
     # Event_and_Attempts
-    pre_time_till_attempt_metrics = get_prev_events_and_time_till_attempt(data)
     print('pre_time_till_attempt_metrics')
     Number_of_games_played_per_type = get_frequency_per_type(data)
     print('Number_of_games_played_per_type')
     Time_spent_on_games_metrics = get_cumulative_time_spent_on_types(data)
     print('Time_spent_on_games_metrics')
-    time_spent_on_diffrent_worlds = get_time_spent_on_diffrent_worlds(data)
     print('time_spent_on_diffrent_worlds')
     Level_reached = substract_level(data)
     print('Level_reached')
     world_time_gametitles_dummies = create_world_time_assesstitle_Dummies(data)
     print('world_time_gametitles_dummies')
-    even_codes_history = get_event_code_history(data)
+
+    words_time_spent = get_time_spent_on_diffrent_worlds(data)
+    print('words_time_spent')
+    events_and_teme_before_attampt = get_prev_events_and_time_till_attempt(data)
+
     Sets = [Number_of_games_played_per_type,
             Time_spent_on_games_metrics,
             world_time_gametitles_dummies,
             Number_of_attemps_and_successes,
             past_assessment_time_events_and_metrics,
-            pre_time_till_attempt_metrics,
-            time_spent_on_diffrent_worlds,
             Level_reached,
             previous_accuracy_metrics,
-            even_codes_history,
+            events_and_teme_before_attampt,
+            words_time_spent,
+
             ag]
     FinalData = reduce(lambda left, right: pd.merge(left, right, how='inner', on=['installation_id', 'game_session']),
                        Sets)
@@ -560,54 +563,11 @@ def create_features(data):
 
 
 ################################################# Create train #########################################################
-wholedata = pd.concat([train,test],ignore_index=True)
-Final = create_features(wholedata)
-cols_to_keep = [
-    'installation_id',
-    'game_session',
-    'Total_Games_played',
-    'Clips__played',
-    'Games_played',
-    'Assessments_played',
-    'Activities_played',
-    'Total_Time_spent',
-    'Time_spent_on_Activities',
-    'Time_spent_on_Games',
-    'Time_spent_on_Assessments',
-    'Average_Time_spent_on_Activities',
-    'Average_Time_spent_on_Games',
-    'Average_Time_spent_on_Assessments',
-    'Average_Time_spent_on_games',
-    'Past_Total_Attempts',
-    'Past_Total_Attempts',
-    'Past_Total_Successes',
-    'Past_Total_Fails',
-    'Past_Assessment_Session_Time',
-    'Past_Assessment_NumberOfEvents',
-    'Level_reached_in_CRYSTALCAVES',
-    'Level_reached_in_MAGMAPEAK',
-    'Level_reached_in_TREETOPCITY',
-    'Total_Level',
-    'Past_Assessment_ag',
-    'Past_Assessment_att',
-    'Past_Assessment_succ',
-    'zeros',
-    'ones',
-    'twos',
-    'threes',
-    'CRYSTALCAVES',
-    'MAGMAPEAK',
-    'TREETOPCITY',
-    'Bird Measurer',
-    'Cart Balancer',
-    'Cauldron Filler',
-    'Chest Sorter',
-    'Mushroom Sorter',
-    'cummean_ag',
-    'accuracy_group']
 
-Final = Final[cols_to_keep]
+Train_dataset = create_features(train)
+Test_dataset = create_features(test)
 
+wholedata = pd.concat([Train_dataset,Test_dataset],ignore_index=True)
 
 Train = pd.merge(train_ids , Final, on = ['installation_id', 'game_session'], how = 'inner')
 Test = pd.merge(test_ids , Final, on = ['installation_id', 'game_session'], how = 'inner')
@@ -616,16 +576,129 @@ X_train= Train.drop('accuracy_group', axis = 1).set_index(['installation_id', 'g
 Y_train= np.asarray(Train['accuracy_group'])
 
 
-X_test= Test.drop('accuracy_group', axis = 1).set_index(['installation_id', 'game_session'])
 To_predict = Test.loc[Test.Assessments_played == Test.groupby('installation_id')['Assessments_played'].transform('max'),['game_session']]
+X_test = Test.loc[~Test.game_session.isin(To_predict.game_session),]
+X_test = X_test.drop('accuracy_group', axis = 1).set_index(['installation_id', 'game_session'])
+
 Y_test= np.asarray(Test.loc[~Test.game_session.isin(To_predict.game_session),'accuracy_group'])
 
 
 
 ################################################# Modelling  things ####################################################
+from sklearn.metrics import  cohen_kappa_score
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(random_state = 42)
+from pprint import pprint
+# Look at parameters used by our current forest
+print('Parameters currently in use:\n')
+pprint(rf.get_params())
 
 
-# submission = pd.DataFrame({"installation_id": X_test.reset_index(1).index.values,
-#                            "accuracy_group": Y_pred_test})
-#
-# submission.to_csv("submission.csv", index=False)
+params = {'bootstrap': True,
+ 'class_weight': None,
+ 'criterion': 'gini',
+ 'max_depth': None,
+ 'max_features': 'auto',
+ 'max_leaf_nodes': None,
+ 'min_impurity_decrease': 0.0,
+ 'min_impurity_split': None,
+ 'min_samples_leaf': 1,
+ 'min_samples_split': 2,
+ 'min_weight_fraction_leaf': 0.0,
+ 'n_estimators': 'warn',
+ 'n_jobs': None,
+ 'oob_score': False,
+ 'random_state': 42,
+ 'verbose': 0,
+ 'warm_start': False}
+
+
+
+from sklearn.model_selection import RandomizedSearchCV
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+pprint(random_grid)
+
+
+
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestClassifier()
+# Random search of parameters, using 3 fold cross validation,
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# Fit the random search model
+rf_random.fit(X_train, Y_train)
+rf_random.best_params_
+{'n_estimators': 1400,
+ 'min_samples_split': 5,
+ 'min_samples_leaf': 4,
+ 'max_features': 'sqrt',
+ 'max_depth': 80,
+ 'bootstrap': True}
+
+def evaluate(model, test_features, test_labels):
+    predictions = model.predict(test_features)
+    kappa  = cohen_kappa_score(test_labels, predictions, weights='quadratic')
+    print('Model Performance')
+    print('kappa: {:0.4f} degrees.'.format(kappa))
+    return kappa
+
+
+base_model = RandomForestClassifier(n_estimators=50, random_state=42)
+base_model.fit(X_train, Y_train)
+base_accuracy = evaluate(base_model, X_test, Y_test)
+
+best_random = RandomForestClassifier(n_estimators = 1400,
+                            min_samples_split =  5,
+                            min_samples_leaf = 4,
+                            max_features = 'sqrt',
+                            max_depth = 80,
+                            bootstrap = True)
+best_random.fit(X_train, Y_train)
+random_accuracy = evaluate(best_random, X_test, Y_test)
+
+print('Improvement of {:0.2f}%.'.format(100 * (random_accuracy - base_accuracy) / base_accuracy))
+
+
+
+
+
+
+
+
+
+model = RandomForestClassifier(n_estimators = 1400,
+                            min_samples_split =  5,
+                            min_samples_leaf = 4,
+                            max_features = 'sqrt',
+                            max_depth = 80,
+                            bootstrap = True)
+model.fit(X_train, Y_train)
+
+predictions = model.predict(X_test)
+
+submission = pd.DataFrame({"installation_id": X_test.reset_index(1).index.values,
+                           "accuracy_group": predictions})
+submission = pd.DataFrame({"installation_id": X_test.reset_index(1).index.values,
+                           "accuracy_group": Y_pred_test})
+
+submission.to_csv("submission.csv", index=False)
